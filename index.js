c9d4edb002c26efd177de32dc9b0c77ff526fdf2
Roni Wellington <roniwellington@gmail.com>
2023-01-15 16:17:02 -0300

    pausa

--- index.js
+++ index.js
@@ -3,8 +3,11 @@
 const menuItem = document.querySelectorAll('.menu-item');
 
 //MESSAGES
+const messagesNotification = document.querySelector('#messages-notification');
 
-
+const messages = document.querySelector('.messages');
+const message = messages.querySelectorAll('.message');
+const messageSearch = document.querySelector('#message-search');
 
 //===================SIDEBAR ================
 
@@ -29,4 +32,33 @@ menuItem.forEach(item => {
 
         }
     })
+})
+
+//===================MESSAGES ================
+//searches chats
+const searchMessage = () => {
+    const val = messageSearch.value.toLowerCase()
+    message.forEach(chat => {
+        let name = chat.querySelectorAll('h5').textContent.toLowerCase();
+        if(name.indexOf(val) != -1){
+            chat.style.display = 'flex';
+        } else {
+            chat.style.display = 'none';
+        }
+    })
+}
+
+
+//search chat
+messageSearch.addEventListener('keyup', searchMessage);
+
+
+//hightlight messages card when messages menu item is clicked
+messagesNotification.addEventListener('click', () => {
+    messages.style.boxShadow = '0 0 1rem var (--color-primary)';
+    messagesNotification.querySelector('.notification-count').style.display = 'none';
+
+    setTimeout(() => {
+        messages.style.boxShadow = 'none';
+    }, 2000);
 })
